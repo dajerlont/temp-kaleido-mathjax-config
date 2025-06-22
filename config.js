edbf327d4fc09a0f847dd50da4282c3e19949a1e
dajerlont <65473505+dajerlont@users.noreply.github.com>
2025-06-22 09:44:39 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+MathJax = {
+  tex: {
+    inlineMath: [['$', '$'], ['\\(', '\\)']],
+    displayMath: [['$$', '$$'], ['\\[', '\\]']]
+  },
+  svg: {
+    fontCache: 'global'
+  },
+  options: {
+    renderActions: {
+      addMenu: []  // Disable the context menu
+    }
+  }
+};
